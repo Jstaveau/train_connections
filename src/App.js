968e474a053a4ab24bd0de3515cb4af8266be710
Jonathan Staveau <jonathanstaveau@MacBook-Pro-de-Jonathan-2.local>
2022-03-30 20:15:31 +0200

    factorisation ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
   useEffect(() => {
     const dateInterval = setInterval(() => {
         setDate(new Date())
-    }, 300000);
+    }, 1000);
 
     return () => {
         clearInterval(dateInterval)
@@ -49,9 +50,44 @@ function App() {
   const [delayCharleroi, setDelayCharleroi] = useState(0)
   const [pastNivelles, setPastNivelles] = useState()
   const [pastCharleroi, setPastCharleroi] = useState()
+  const [delayRawNivelles, setDelayRawNivelles] = useState()
+  const [delayRawCharleroi, setDelayRawCharleroi] = useState()
+
   let canceledCharleroi = 0
   let canceledNivelles = 0
 
+  //functions
+  //average
+  const average = (ville) => {
+    let trainsNivelles = ville.connection.length
+    let ridingTrainNivelle = 0;
+    ville.connection.forEach(e => {
+      if (dateTime + 3600000 >= e.departure.time * 1000) {
+        ridingTrainNivelle += 1
+      }
+    });
+    let result = (ridingTrainNivelle/trainsNivelles * 100).toFixed(2)
+    return result
+  }
+  //delay
+  const delay = (ville) => {
+    let trainsNivelles = ville.connection.length
+    let delayTrainNivelle = 0
+    ville.connection.forEach(e => {
+      if (dateTime + 3600000 >= e.departure.time * 1000 && (e.departure.delay > 0)) {
+        delayTrainNivelle += parseInt(e.departure.delay)
+        }
+    });
+    let delayRawNivelles = delayTrainNivelle/trainsNivelles
+    return parseInt(delayRawNivelles)
+  }
+  //format delay
+  const formatDelay = (delay) => {
+    let hourDelay = new Date(delay * 1000)
+    return hourDelay.getMinutes()
+  }
+
+  //useEffect to update data
   useEffect(() => {
     fetch('https://api.irail.be/connections/?from=charleroi-sud&to=nivelles&format=json&date=' + dateURL + '&time=' + hour)
     .then(response => response.json())
@@ -80,48 +116,35 @@ function App() {
     })
 
     // average nivelles
-    let trainsNivelles = nivelles.connection.length
-    let ridingTrainNivelle = 0;
-    nivelles.connection.forEach(e => {
-      if (dateTime + 3600000 >= e.departure.time * 1000) {
-        ridingTrainNivelle += 1
-      }
-    });
-    setAverageNivelle((ridingTrainNivelle/trainsNivelles * 100).toFixed(2))
+    setAverageNivelle(average(nivelles))
     
     //average charleroi
-    let trainsCharleroi = charleroi.connection.length
-    let ridingTrainCharleroi = 0;
-    charleroi.connection.forEach(e => {
-      if (dateTime + 3600000 >= e.departure.time * 1000) {
-        ridingTrainCharleroi += 1
-      }
-    });
-    setAverageCharleroi((ridingTrainNivelle/trainsCharleroi * 100).toFixed(2))
+    setAverageCharleroi(average(charleroi))
     
     //delay nivelles
-    let delayTrainNivelle = 0
-    nivelles.connection.forEach(e => {
-      if (dateTime + 3600000 >= e.departure.time * 1000 && (e.departure.delay > 0)) {
-        delayTrainNivelle += parseInt(e.departure.delay)
-      }
-    });
-    let delayRawNivelles = delayTrainNivelle/trainsNivelles
+
+    // let delayTrainNivelle = 0
+    // nivelles.connection.forEach(e => {
+    //   if (dateTime + 3600000 >= e.departure.time * 1000 && (e.departure.delay > 0)) {
+    //     delayTrainNivelle += parseInt(e.departure.delay)
+    //   }
+    // });
+    // let delayRawNivelles = delayTrainNivelle/trainsNivelles
+    setDelayRawNivelles(delay(nivelles))
+
 
     //delay charleroi
-    let delayTrainCharleroi = 0
-    charleroi.connection.forEach(e => {
-      if (dateTime + 3600000 >= e.departure.time * 1000 && (e.departure.delay > 0)) {
-        delayTrainCharleroi += parseInt(e.departure.delay)
-      }
-    });
-    let delayRawCharleroi = delayTrainCharleroi/trainsCharleroi
+    // let delayTrainCharleroi = 0
+    // charleroi.connection.forEach(e => {
+    //   if (dateTime + 3600000 >= e.departure.time * 1000 && (e.departure.delay > 0)) {
+    //     delayTrainCharleroi += parseInt(e.departure.delay)
+    //   }
+    // });
+    // let delayRawCharleroi = delayTrainCharleroi/trainsCharleroi
+    setDelayRawCharleroi(delay(charleroi))
+
 
     //function format delay
-    const formatDelay = (delay) => {
-      let hourDelay = new Date(delay * 1000)
-      return hourDelay.getMinutes()
-    }
     setDelayNivelle(formatDelay(delayRawNivelles))
     setDelayCharleroi(formatDelay(delayRawCharleroi))
     
@@ -139,19 +162,45 @@ function App() {
         canceledNivelles += 1
       }
     });
-
-
     
   }, [date])
 
   return (
-    <div className="App">
-      <p>Pourcentage de train en marche durant la prochaine heure : {averageNivelle} % & {averageCharleroi} %</p>
-      <p>Retards nivelle : {delayNivelle}min</p>
-      <p>Retards Charleroi : {delayCharleroi}min</p>
-      <p>Trains annulés Nivelles: {canceledNivelles}</p>
-      <p>Trains annulés Charleroi : {canceledCharleroi}</p>
-      <h1>{dd + "/"+ mm + "/"+ yy} = {dateTime} heure = {hour}</h1>
+    <div className="App container mt-5 bg-dark p-3 rounded rounded-3">
+      <div className="d-flex justify-content-around mt-5 m-auto">
+        <div>
+          <h5 className="text-white">Pourcentage des trains en marche partants de Nivelles</h5>
+          <div className="progress mb-3">
+            <div className="progress-bar bg-danger" role="progressbar"  aria-valuenow="25" style={{width : averageNivelle + '%'}} aria-valuemin="0" aria-valuemax="100">{averageNivelle}%</div>
+          </div>
+        </div>
+        <div>
+          <h5 className="text-white">Pourcentage des trains en marche partants de Charleroi</h5>
+          <div className="progress">
+            <div className="progress-bar bg-danger" role="progressbar"  aria-valuenow="25" style={{width : averageCharleroi + '%'}} aria-valuemin="0" aria-valuemax="100">{averageCharleroi}%</div>
+          </div>
+        </div>
+      </div>
+      <div className="text-light mt-5 d-flex justify-content-around">
+        <div>
+          <p>Retard moyen pour le trains partants de Nivelles</p>
+          <p className="text-center bg-light text-danger p-3">{delayNivelle} minute(s)</p>
+        </div>
+        <div>
+          <p>Retard moyen pour le trains partants de Charleroi</p>
+          <p className="text-center bg-light text-danger p-3">{delayCharleroi} minute(s)</p>
+        </div>
+      </div>
+      <div className="text-light mt-5 mb-5 d-flex justify-content-around">
+        <div>
+          <p>Trains annulés durant les deux dernières heures à Nivelles</p>
+          <p className="text-center bg-light text-danger p-3">{canceledNivelles} train(s)</p>
+        </div>
+        <div>
+          <p>Trains annulés durant les deux dernières heures à Nivelles</p>
+          <p className="text-center bg-light text-danger p-3">{canceledCharleroi} train(s)</p>
+        </div>
+      </div>
     </div>
   );
 }
